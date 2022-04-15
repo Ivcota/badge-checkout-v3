d3e9d5965a270890dc3f1911d409c863eb363e3a
Ivcota <ivcotad@gmail.com>
2022-04-15 09:31:56 -0700

    refactor

--- schema.ts
+++ schema.ts
@@ -1,38 +1,67 @@
-// Code copied (with some modifications) from the Keystone 6 "with-auth" example
-// See.. https://github.com/keystonejs/keystone/tree/master/examples/with-auth
+import { Lists } from ".keystone/types";
+import { list } from "@keystone-6/core";
+import {
+  checkbox,
+  password,
+  relationship,
+  text,
+} from "@keystone-6/core/fields";
 
-import { list } from '@keystone-6/core';
-import { checkbox, password, relationship, text, timestamp, select } from '@keystone-6/core/fields';
-
-export const lists = {
-  Task: list({
+export const lists: Lists = {
+  User: list({
     fields: {
-      label: text({ isRequired: true }),
-      priority: select({
-        dataType: 'enum',
-        options: [
-          { label: 'Low', value: 'low' },
-          { label: 'Medium', value: 'medium' },
-          { label: 'High', value: 'high' },
-        ],
-      }),
-      isComplete: checkbox(),
-      assignedTo: relationship({ ref: 'Person.tasks', many: false }),
-      finishBy: timestamp(),
+      email: text({ validation: { isRequired: true }, isIndexed: "unique" }),
+      password: password({ validation: { isRequired: true } }),
     },
   }),
-  Person: list({
+  Attendant: list({
     fields: {
-      name: text({ isRequired: true }),
-      // Added an email and password pair to be used with authentication
-      // The email address is going to be used as the identity field, so it's
-      // important that we set both isRequired and isUnique
-      email: text({ isRequired: true, isUnique: true, isIndexed: 'unique' }),
-      // The password field stores a hash of the supplied password, and
-      // we want to ensure that all people have a password set, so we use
-      // the isRequired flag.
-      password: password({ isRequired: true }),
-      tasks: relationship({ ref: 'Task.assignedTo', many: true }),
+      firstName: text({
+        validation: { isRequired: true },
+      }),
+      lastName: text({
+        validation: { isRequired: true },
+      }),
+      email: text({
+        validation: { isRequired: true },
+        isIndexed: "unique",
+      }),
+      phone: text({
+        validation: { isRequired: true },
+      }),
+      congregation: relationship({ ref: "Congregation.publishers" }),
+      keyman: relationship({ ref: "Department.keyman" }),
+      department: relationship({ ref: "Department.attendants" }),
+      badgeIsCheckedIn: checkbox({ defaultValue: false }),
+    },
+    ui: {
+      labelField: "lastName",
+      listView: {
+        initialColumns: [
+          "lastName",
+          "firstName",
+          "phone",
+          "badgeIsCheckedIn",
+          "department",
+        ],
+      },
+    },
+  }),
+  Department: list({
+    fields: {
+      name: text({
+        validation: { isRequired: true },
+      }),
+      keyman: relationship({ ref: "Attendant.keyman" }),
+      attendants: relationship({ ref: "Attendant.department", many: true }),
+    },
+  }),
+  Congregation: list({
+    fields: {
+      name: text({
+        validation: { isRequired: true },
+      }),
+      publishers: relationship({ ref: "Attendant.congregation", many: true }),
     },
   }),
 };
